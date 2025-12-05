533fe6af5e7eac1b57b30146343d96384ac845d3
pao243685 <243685@ids.upchiapas.edu.mx>
2025-12-05 15:58:32 -0600

    deploy

--- src/main/kotlin/config/DatabaseFactory.kt
+++ src/main/kotlin/config/DatabaseFactory.kt
@@ -13,6 +13,7 @@ object DatabaseFactory {
             driverClassName = "org.postgresql.Driver"
             jdbcUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/huellitas_callejeras"
             username = System.getenv("DB_USER") ?: "postgres"
+            //password = System.getenv("DB_PASSWORD") ?: "Perlita1610"
             password = System.getenv("DB_PASSWORD") ?: "huellitas123"
             maximumPoolSize = 10
             isAutoCommit = false
