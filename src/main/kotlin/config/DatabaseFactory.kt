3a3bd51103b2ac473080cafbf043920a6efb3be9
pao243685 <243685@ids.upchiapas.edu.mx>
2025-12-07 00:06:05 -0600

    config bd

--- src/main/kotlin/config/DatabaseFactory.kt
+++ src/main/kotlin/config/DatabaseFactory.kt
@@ -11,12 +11,12 @@ object DatabaseFactory {
     fun init() {
         val config = HikariConfig().apply {
             driverClassName = "org.postgresql.Driver"
-//            jdbcUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://34.195.100.95:5432/huellitas_callejeras"
-            jdbcUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://127.0.0.1:5432/huellitas_callejeras5"
+            jdbcUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://34.195.100.95:5432/huellitas_callejeras"
+//            jdbcUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://127.0.0.1:5432/huellitas_callejeras5"
             username = System.getenv("DB_USER") ?: "postgres"
-            password = System.getenv("DB_PASSWORD") ?: "Perlita1610"
+//            password = System.getenv("DB_PASSWORD") ?: "Perlita1610"
 //            password = System.getenv("DB_PASSWORD") ?: "123456"
-            //password = System.getenv("DB_PASSWORD") ?: "huellitas123"
+            password = System.getenv("DB_PASSWORD") ?: "huellitas123"
 //            password = System.getenv("DB_PASSWORD") ?: "2006"
             maximumPoolSize = 10
             isAutoCommit = false
